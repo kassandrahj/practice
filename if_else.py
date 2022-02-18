d2a4344a8d46cbd1fa7d384488933034ce81ee33
kassandrahj <jonahkassandrah@gmail.com>
2022-02-18 16:37:59 +0100

    file reading

--- if_else.py
+++ if_else.py
@@ -1,12 +1,59 @@
-#a program that asks for your details and prints out your wage
-print('welcome to our services')
-hours_worked = float(input("enter the number of hours worked for the month"))
-rate_A = 0.5
-rate_b = 0.31
-wage = rate_A*hours_worked
-print(wage)
-if hours_worked >= 38:
-    print('your wage is:{}'.format(wage))
-else:
-    wage = rate_b*hours_worked
-    print ('your wage is:{}'.format(wage))
+# #a program that asks for your details and prints out your wage
+# print('welcome to our services')
+# hours_worked = float(input("enter the number of hours worked for the month"))
+# rate_A = 0.5
+# rate_b = 0.31
+# wage = rate_A*hours_worked
+# print(wage)
+# if hours_worked >= 38:
+#     print('your wage is:{}'.format(wage))
+# else:
+#     wage = rate_b*hours_worked
+#     print ('your wage is:{}'.format(wage))
+
+
+# greeting everyone individually
+# name = 'kassandrah ameer amrah'
+# n=name[0:10]
+# print('{}, hello welcome home'.format(n))
+# x =len(n)
+# print('the length of your name is {}'.format(x))
+# w=name[10:16]
+# print('{}, hello welcome home'.format(w))
+# y =len(w)
+# print('the length of your name is {}'.format(y))
+# z=name[16:]
+# print('{}, hello welcome home'.format(z))
+# b=len(z)
+# print('the length of your name is {}'.format(b))
+
+# just some code on functions
+# import math
+# import random
+# salary = 56
+# new_salary =salary*math.factorial (3)
+# print(new_salary)
+# rate =int(input('enter the proposed rate'))
+# r = (math.pow(rate,2))
+# print(r)
+# # rand =random.random
+# # print(rand)
+# for n in range(90) :
+#     x = random.random()
+#     print(x)
+# b = random.randint(90,1000)
+# print(b)
+# import math
+# import random
+# name = ['kassandrah' ,'ameer' ,'amrah']
+# c = random.choice(name)
+# print('{} you are most welcome home'.format(c))
+# n=len(c)
+# print('the lenght of your name is {}'.format(n))
+
+# def print_lyrics():
+#     w = 'water, e no get enemy'
+#     c= w*4
+#     print(c)
+# d= print_lyrics
+# print(d)
